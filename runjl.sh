6b17b80d1b0b0a910050ce159ba526195ad1baf4
Florian Oswald <florian.oswald@unit.it>
2026-02-24 16:20:12 +0100

    add bash

--- runjl.sh
+++ runjl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # file name: runjl.sh
 
+source ~/.bashrc
+
 echo "this job will run make julia print hi"
 
 julia -e "println(\"hi from the julia command line\")"
--- runjl2.sh
+++ runjl2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # file name: runjl.sh
 
+source ~/.bashrc
+
 echo "this job will run a julia script"
 
 julia script.jl
